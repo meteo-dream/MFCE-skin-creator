e2a30efc25ee31622016b2a5daf82b13c9166d26
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-07-02 03:16:34 +0900

    add binaries to be used by release build

---
+++ custom.py
@@ -0,0 +1,34 @@
+target="template_release"
+debug_symbols="no"
+lto="thin"
+arch="x86_64"
+optimize="size_extra"
+
+production="yes"
+disable_3d="yes"
+
+deprecated="yes" 
+vulkan="no"      # Disables the Vulkan driver (used in Forward+/Mobile Renderers)
+use_volk="no"    # Disables more Vulkan stuff
+openxr="no"      # Disables Virtual Reality/Augmented Reality stuff
+minizip="yes"
+d3d12="no"
+
+accesskit="no"   # Disables screen reader
+winrt="no"
+angle="no"
+disable_navigation_2d="yes"
+disable_navigation_3d="yes"
+disable_physics_2d="yes"
+disable_physics_3d="yes"
+disable_xr="yes"
+
+module_text_server_adv_enabled="yes"
+
+modules_enabled_by_default="no"
+module_freetype_enabled="yes"
+module_gdscript_enabled="yes"
+module_svg_enabled="yes"
+module_webp_enabled="yes"
+module_zip_enabled="yes"
+module_msdfgen_enabled="no"
